765e5c3cd50872dee17ffae4c0ef540f94b2538a
bellujrb <98968823+bellujrb@users.noreply.github.com>
2025-06-20 03:29:35 -0300

    feat: add HowItWorks component to enhance user onboarding experience

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -2,13 +2,14 @@ import Header from '@/components/Header';
 import HeroSection from '@/components/HeroSection';
 import FAQ from '@/components/FAQ';
 import Footer from '@/components/Footer';
+import HowItWorks from '@/components/HowItWorks';
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Header />
       <HeroSection />
-      {/* <HowItWorks /> */}
+      <HowItWorks />
       <FAQ />
       <Footer />
     </main>
